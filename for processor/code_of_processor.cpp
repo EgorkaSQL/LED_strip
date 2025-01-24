64a9ed120964003644033a6a0e47c7d0c9bbb763
Flaskort <97551291+EgorkaSQL@users.noreply.github.com>
2025-01-24 22:27:10 +0300

    Update code_of_processor.cpp
    - Были прописаны и протестированы переключения цветов в меню "Цвета";
    - Переписаны 3 режима: Blink, Fade, Phonk

--- for processor/code_of_processor.cpp
+++ for processor/code_of_processor.cpp
@@ -1,179 +1,295 @@
 #include "BluetoothSerial.h"
 
-#undef LED_BUILTIN
-#define LED_BUILTIN 14
-#define LED_CHANNEL 0
-#define LED_RESOLUTION 8
-#define LED_FREQ 5000
+#define PIN_R 14
+#define PIN_G 26
+#define PIN_B 33
 
 BluetoothSerial SerialBT;
 
-bool blinkEnabled = false;
-bool fadeEnabled = false;
-bool phonkEnabled = false;
-bool sosEnabled = false;
+bool isBlinking = false;
+bool isFading = false;
+bool isPhonk = false;
+int fadeDirection = 1;
+int brightness = 0;
+const int fadeSpeed = 5;
 
-unsigned long previousMillis = 0;
-const unsigned long blinkInterval = 200;
-const unsigned long longBlinkInterval = 500;
-const unsigned long sosPauseInterval = 3000;
-bool ledState = false;
+unsigned long phonkStartTime = 0;
+int phonkPhase = 0;
+unsigned long phonkLastSwitchTime = 0;
+const unsigned long phonkInterval = 200;
 
-int phonkStep = 0;
-int sosStep = 0;
+bool redState = false;
+bool greenState = false;
+bool blueState = false;
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  ledcSetup(LED_CHANNEL, LED_FREQ, LED_RESOLUTION);
-  ledcAttachPin(LED_BUILTIN, LED_CHANNEL);
+void resetPins() 
+{
+  digitalWrite(PIN_R, LOW);
+  digitalWrite(PIN_G, LOW);
+  digitalWrite(PIN_B, LOW);
 
-  ledcWrite(LED_CHANNEL, 0);
+  pinMode(PIN_R, OUTPUT);
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+}
 
-  Serial.begin(115200);
-  delay(1000);
-  SerialBT.begin("ESP32_Bluetooth");
-  Serial.println("ESP32 запущен!");
-  SerialBT.println("Bluetooth готов!");
+void setup() 
+{ 
+  pinMode(PIN_R, OUTPUT); 
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+
+  digitalWrite(PIN_G, LOW);
+  digitalWrite(PIN_B, LOW);
+  digitalWrite(PIN_R, LOW);
+
+  SerialBT.begin("ESP32_LED_Control");
+  Serial.println("Bluetooth initialized");
 }
 
 void loop() {
-  if (SerialBT.available()) {
+  if (SerialBT.available()) 
+  {
     char command = SerialBT.read();
-    Serial.print("Получено: ");
-    Serial.println(command);
 
-    if (command == 'B') {
-      String brightnessValue = "";
-      while (SerialBT.available()) {
-        char ch = SerialBT.read();
-        if (isDigit(ch)) {
-          brightnessValue += ch;
-        } else {
+    // Синий цвет
+    if (command == 'M')
+    {
+      Serial.println("Включен синий цвет");
+      pinMode(PIN_R, INPUT);
+      digitalWrite(PIN_R, HIGH);
+      pinMode(PIN_B, OUTPUT);
+      pinMode(PIN_G, INPUT);
+    }
+    if (command == 'E') 
+    {
+      Serial.println("Выключен синий цвет");
+      resetPins();
+    }
+
+    // Красный цвет
+    if (command == 'Z')
+    {
+      Serial.println("Включен красный цвет");
+      pinMode(PIN_R, OUTPUT);
+      pinMode(PIN_G, INPUT);
+      pinMode(PIN_B, INPUT);
+    }
+    if (command == 'X') 
+    {
+      Serial.println("Выключен красный цвет");
+      resetPins();
+    }
+
+    // Зеленый цвет
+    if (command == 'Q')
+    {
+      Serial.println("Включен зеленый цвет");
+      pinMode(PIN_G, OUTPUT);
+      digitalWrite(PIN_R, HIGH);
+      pinMode(PIN_R, INPUT);
+      pinMode(PIN_B, INPUT);
+    }
+    if (command == 'N') 
+    {
+      Serial.println("Выключен зеленый цвет");
+      resetPins();
+    }
+
+    // Синий и красный цвет
+    if (command == 'O')
+    {
+      Serial.println("Включен синий и красный цвет");
+      pinMode(PIN_R, OUTPUT);
+      pinMode(PIN_B, OUTPUT);
+      digitalWrite(PIN_R, HIGH);
+      pinMode(PIN_G, INPUT);
+    }
+    if (command == 'R') 
+    {
+      Serial.println("Выключен синий и красный цвет");
+      resetPins();
+    }
+
+    // Синий и зеленый цвет
+    if (command == '2')
+    {
+      Serial.println("Включен синий и зеленый цвет");
+      pinMode(PIN_G, OUTPUT);
+      pinMode(PIN_B, OUTPUT);
+      pinMode(PIN_R, INPUT);
+    }
+    if (command == 'V') 
+    {
+      Serial.println("Выключен синий и зеленый цвет");
+      resetPins();
+    }
+
+    // Зеленый и красный цвет
+    if (command == 'b')
+    {
+      Serial.println("Включен зеленый и красный цвет");
+      pinMode(PIN_G, OUTPUT);
+      pinMode(PIN_R, OUTPUT);
+      pinMode(PIN_B, INPUT);
+    }
+    if (command == 'a') 
+    {
+      Serial.println("Выключен зеленый и красный цвет");
+      resetPins();
+    }
+
+    // Режимы
+    // Blink
+    if (command == '3')
+    {
+      Serial.println("Включен Blink");
+      isBlinking = true;
+    }
+    if (command == '4') 
+    {
+      Serial.println("Выключен Blink");
+      isBlinking = false;
+      resetPins();
+    }
+
+    // Fade
+    if (command == '5')
+    {
+      Serial.println("Включен Fade");
+      isFading = true;
+      brightness = 0;
+    }
+    if (command == '6') 
+    {
+      Serial.println("Выключен Fade");
+      isFading = false;
+      resetPins();
+    }
+
+    // Phonk
+    if (command == '7') 
+    {
+      Serial.println("Включен режим Phonk");
+      resetPins();
+      isPhonk = true;
+      phonkStartTime = millis();
+      phonkLastSwitchTime = millis();
+      phonkPhase = 0;
+    }
+    if (command == '8') 
+    {
+      Serial.println("Выключен режим Phonk");
+      isPhonk = false;
+      resetPins();
+    }
+  }
+
+  if (isBlinking) 
+  {
+    pinMode(PIN_B, OUTPUT);
+    pinMode(PIN_G, OUTPUT);
+    pinMode(PIN_R, OUTPUT);
+    delay(1000);
+
+    digitalWrite(PIN_R, LOW);
+    digitalWrite(PIN_G, LOW);
+    digitalWrite(PIN_B, LOW);
+    pinMode(PIN_B, INPUT);
+    pinMode(PIN_G, INPUT);
+    pinMode(PIN_R, INPUT);
+    delay(1000);
+  }
+
+  if (isFading)
+  {
+    analogWrite(PIN_R, brightness);
+    analogWrite(PIN_G, brightness);
+    analogWrite(PIN_B, brightness);
+
+    brightness += fadeDirection * fadeSpeed;
+
+    if (brightness <= 0 || brightness >= 255) 
+    {
+      fadeDirection = -fadeDirection;
+    }
+
+    delay(30);
+  }
+
+  if (isPhonk) 
+  {
+    unsigned long currentTime = millis();
+
+    if (currentTime - phonkStartTime > 2000) 
+    {
+      phonkStartTime = currentTime;
+      phonkPhase = (phonkPhase + 1) % 3;
+    }
+
+    if (currentTime - phonkLastSwitchTime > phonkInterval) 
+    {
+      phonkLastSwitchTime = currentTime;
+
+      switch (phonkPhase) 
+      {
+        case 0:  // Фаза 0: мигание красного
+          if (redState) 
+          {
+            pinMode(PIN_R, INPUT);
+            redState = false;
+          } 
+          else 
+          {
+            pinMode(PIN_R, OUTPUT);
+            digitalWrite(PIN_R, HIGH);
+            redState = true;
+          }
           break;
-        }
+
+        case 1:  // Фаза 1: мигание красного и синего
+          if (redState || blueState) 
+          {
+            pinMode(PIN_R, INPUT);
+            pinMode(PIN_B, INPUT);
+            redState = false;
+            blueState = false;
+          } 
+          else 
+          {
+            pinMode(PIN_R, OUTPUT);
+            digitalWrite(PIN_R, HIGH);
+            redState = true;
+
+            pinMode(PIN_B, OUTPUT);
+            digitalWrite(PIN_B, HIGH);
+            blueState = true;
+          }
+          break;
+
+        case 2:  // Фаза 2: мигание зеленого и красного
+          if (redState || greenState) 
+          {
+            pinMode(PIN_R, INPUT);
+            pinMode(PIN_G, INPUT);
+            redState = false;
+            greenState = false;
+          } 
+          else 
+          {
+            pinMode(PIN_R, OUTPUT);
+            digitalWrite(PIN_R, HIGH);
+            redState = true;
+
+            pinMode(PIN_G, OUTPUT);
+            digitalWrite(PIN_G, HIGH);
+            greenState = true;
+          }
+          break;
+
+        default:
+          break;
       }
-
-      int brightness = brightnessValue.toInt();
-      brightness = constrain(brightness, 0, 255);
-
-      ledcWrite(LED_CHANNEL, brightness);
-      Serial.print("Яркость изменена на: ");
-      Serial.println(brightness);
-    }
-
-    if (command == '3') {
-      blinkEnabled = true;
-      fadeEnabled = false;
-      sosEnabled = false;
-      Serial.println("Мигание включено");
-    } else if (command == '4') {
-      blinkEnabled = false;
-      ledcWrite(LED_CHANNEL, 0);
-      Serial.println("Мигание выключено");
-    } else if (command == '5') {
-      fadeEnabled = true;
-      blinkEnabled = false;
-      sosEnabled = false;
-      Serial.println("Затухание включено");
-    } else if (command == '6') {
-      fadeEnabled = false;
-      ledcWrite(LED_CHANNEL, 0);
-      Serial.println("Затухание выключено");
-    } else if (command == '7') {
-      phonkEnabled = true;
-      sosEnabled = false;
-      blinkEnabled = false;
-      fadeEnabled = false;
-      phonkStep = 0;
-      ledState = false;
-      previousMillis = millis();
-      Serial.println("Фонк включено");
-    } else if (command == '8') {
-      phonkEnabled = false;
-      ledcWrite(LED_CHANNEL, 0);
-      Serial.println("Фонк выключено");
-    } else if (command == '9') {
-      sosEnabled = true;
-      blinkEnabled = false;
-      fadeEnabled = false;
-      phonkEnabled = false;
-      sosStep = 0;
-      ledcWrite(LED_CHANNEL, 0);
-      previousMillis = millis();
-      Serial.println("Режим SOS включён");
-    } else if (command == 'W') {
-      sosEnabled = false;
-      ledcWrite(LED_CHANNEL, 0);
-      Serial.println("Режим SOS выключен");
-    }
-  }
-
-  if (blinkEnabled) {
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= blinkInterval) {
-      previousMillis = currentMillis;
-      ledState = !ledState;
-      ledcWrite(LED_CHANNEL, ledState ? 255 : 0);
-    }
-  }
-
-  if (fadeEnabled) {
-    for (int brightness = 0; brightness <= 255; brightness++) {
-      ledcWrite(LED_CHANNEL, brightness);
-      delay(10);
-    }
-    for (int brightness = 255; brightness >= 0; brightness--) {
-      ledcWrite(LED_CHANNEL, brightness);
-      delay(10);
-    }
-  }
-
-  if (phonkEnabled) {
-    unsigned long currentMillis = millis();
-    unsigned long interval = (phonkStep < 3) ? longBlinkInterval : blinkInterval;
-
-    if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-      ledState = !ledState;
-      ledcWrite(LED_CHANNEL, ledState ? 255 : 0);
-
-      if (!ledState) {
-        phonkStep++;
-      }
-
-      if (phonkStep >= 9) {
-        phonkStep = 0;
-      }
-    }
-  }
-
-  if (sosEnabled) {
-    unsigned long currentMillis = millis();
-    unsigned long interval;
-
-    if (sosStep >= 18) {
-      interval = sosPauseInterval;
-      if (currentMillis - previousMillis >= interval) {
-        previousMillis = currentMillis;
-        sosStep = 0;
-      }
-      return;
-    } 
-
-    if (sosStep % 2 == 0) {
-      interval = (sosStep / 6 == 1) ? longBlinkInterval : blinkInterval;
-      ledState = true;
-    } else {
-      interval = blinkInterval;
-      ledState = false;
-    }
-
-    if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-
-      ledcWrite(LED_CHANNEL, ledState ? 255 : 0);
-      sosStep++;
     }
   }
 }
